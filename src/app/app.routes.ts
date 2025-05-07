c324890d659cce702131cb48b2c12d319d4e6d7a
fcampoa <felixcampoa@gmail.com>
2025-05-07 15:33:12 -0700

    site navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './admin/components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'site', pathMatch: 'full' },
+    { path: 'site', loadChildren: () => import('./site/site.routes').then(m => m.routes) },
+    { path: 'admin', component: MainComponent, loadChildren: () => import('./admin/admin.routes').then(m => m.routes) },
+];
---
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  imports: [ MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    RouterModule],
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.scss']
+})
+export class SideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
