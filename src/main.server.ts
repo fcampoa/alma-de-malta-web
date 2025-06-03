5cba30853b9771463459004973bcf4bd3afa3c96
fcampoa <felixcampoa@gmail.com>
2025-06-02 17:13:44 -0700

    deploy test

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { appConfig } from './app/app.config';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+export function run(): Promise<void> {
+  return bootstrapApplication(AppComponent, appConfig)
+    .then(() => {})
+    .catch(err => console.error(err));
+}
 
-export default bootstrap;
+export default run;
