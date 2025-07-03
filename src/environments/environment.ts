770477f897ffa5d8ac6167327d82925b60a4a34f
fcampoa <felixcampoa@gmail.com>
2025-07-02 23:05:25 -0700

    redirect logout

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
         clientId: 'Lyj9UZ5xFp15SsB3GJcIXKhoHcHEX1SE',
         audience: 'https://dev-g6ghivncjbkdrr86.us.auth0.com/api/v2/',
         redirectUri: window.location.origin,
+        logoutUrl: window.location.origin,
         scope: 'openid profile email'
     }
 }
